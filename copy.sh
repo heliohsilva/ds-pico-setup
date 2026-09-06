696e286b07b1065c65951a4039d383550579be61
Helio Henrique Medeiros Silva <helio.medeiros_silva@outlook.com>
2026-09-06 13:29:50 -0300

    finished the workflow

--- copy.sh
+++ copy.sh
@@ -1,6 +1,10 @@
 D="ds-pico-setup:/app";
 
+mkdir -p firmware;
 mkdir -p setup;
+mkdir -p setup/roms;
+
+docker cp $D/dspico-firmware/build/DSpico.uf2 ./firmware/;
 
 docker cp $D/pico-launcher/_pico ./setup/;
 docker cp $D/pico-launcher/LAUNCHER.nds ./setup/_picoboot.nds;
